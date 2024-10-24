7477404e25dc6c12f015d45621e63f1f11295b9a
pierregee <pierre.gabaldon@gmail.com>
2024-10-25 05:28:32 +0800

    add loaders

--- apps/web/src/components/Rate.tsx
+++ apps/web/src/components/Rate.tsx
@@ -1,24 +1,35 @@
 import useConversionRate from "@/hooks/conversionRate";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function Rate() {
   const { isPending, data, isFetching, failureReason } = useConversionRate();
-  const hasError = failureReason !== null; // isError is not reliable based on integration tests
+  const hasError = failureReason !== null;
+
+  let content;
+
+  if (hasError) {
+    content = <div className="error">An error has occurred</div>;
+  } else if (isPending || isFetching) {
+    content = (
+      <div className="loading">
+        <Skeleton width="310px" height="70px" />
+      </div>
+    );
+  } else if (data) {
+    content = (
+      <div className="data">
+        <p id="conversion-rate" className="rate">
+          1 ETH = {data.conversionRate} pufETH
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div className="container">
-      <div className="card">
-        {isPending && !hasError && <div className="loading">Loading...</div>}
-        {hasError && <div className="error">An error has occurred</div>}
-        {data && (
-          <div className="data">
-            <p id="conversion-rate" className="rate">
-              1 ETH = {data.conversionRate} pufETH
-            </p>
-            {isFetching && <div className="fetching">Updating...</div>}
-          </div>
-        )}
-      </div>
+      <div className="card">{content}</div>
     </div>
   );
 }
