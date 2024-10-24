c909682e180286c83c861d57f6065d78b77ab8e9
pierregee <pierre.gabaldon@gmail.com>
2024-10-24 11:32:41 +0800

    feat(core): add tests

--- apps/web/conversion-rate-tracker-app/src/components/Rate.tsx
+++ apps/web/conversion-rate-tracker-app/src/components/Rate.tsx
@@ -2,19 +2,21 @@ import useConversionRate from "@/hooks/conversionRate";
 import React from "react";
 
 export default function Rate() {
-  const { isPending, error, data, isFetching } = useConversionRate();
+  const { isPending, data, isFetching, failureReason } = useConversionRate();
+  const hasError = failureReason !== null; // isError is not reliable based on integration tests
 
+  console.log({ failureReason });
   return (
     <div className="container">
       <div className="card">
-        {isPending && <div className="loading">Loading...</div>}
-        {error && (
-          <div className="error">An error has occurred: {error.message}</div>
-        )}
+        {isPending && !hasError && <div className="loading">Loading...</div>}
+        {hasError && <div className="error">An error has occurred</div>}
         {data && (
           <div className="data">
             <h2 className="title">Conversion Rate</h2>
-            <p className="rate">{data.conversionRate}</p>
+            <p id="conversion-rate" className="rate">
+              {data.conversionRate}
+            </p>
             {isFetching && <div className="fetching">Updating...</div>}
           </div>
         )}
