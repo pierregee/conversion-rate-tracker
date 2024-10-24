eed24f96ede14f310803f72f7ec8ac8b359d14cb
pierregee <pierre.gabaldon@gmail.com>
2024-10-24 22:48:09 +0800

    revert changes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,22 +1,21 @@
 "use client";
 import React, { Suspense } from "react";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// import Rate from "@/components/Rate";
-// import NetworkDropdown from "@/components/NetworkDropdown";
+import Rate from "@/components/Rate";
+import NetworkDropdown from "@/components/NetworkDropdown";
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div>test</div>
-      {/* <QueryClientProvider client={queryClient}> */}
-      {/* <ReactQueryDevtools /> */}
-      {/* <NetworkDropdown /> */}
-      {/* <Rate /> */}
-      {/* </QueryClientProvider> */}
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <NetworkDropdown />
+        <Rate />
+      </QueryClientProvider>
     </Suspense>
   );
 }
