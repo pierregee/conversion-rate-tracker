5e330bb6c465e83b4280d36acfe7b0ffc458a122
pierregee <pierre.gabaldon@gmail.com>
2024-10-24 14:54:28 +0800

    cleanup package

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,20 +1,17 @@
 "use client";
-import React from 'react'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import React from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import Rate from '@/components/Rate';
+import Rate from "@/components/Rate";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
       <Rate />
-  </QueryClientProvider>
+    </QueryClientProvider>
   );
 }
