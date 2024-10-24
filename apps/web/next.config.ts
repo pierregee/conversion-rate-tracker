e798a9b5e9b615155e8d6e4b7030ebc592e0174b
pierregee <pierre.gabaldon@gmail.com>
2024-10-25 03:53:41 +0800

    fix yml

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_API_BASE_URL:
+      "https://puffer-vault-rate-api.netlify.app/.netlify/functions/index",
   },
 };
 
